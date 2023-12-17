046c50e38ee06e2b6ea152c46eea3aeeea2cd3bc
asdwer <94925215+asdwer-dev@users.noreply.github.com>
2023-12-17 23:41:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Пустой файл script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const matrixBg = document.createElement('div');
+    matrixBg.classList.add('matrix-bg');
+    document.body.appendChild(matrixBg);
+
+    const rows = 30;
+    const cols = 30;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const matrixElement = document.createElement('span');
+            matrixElement.classList.add('matrix-element');
+            matrixElement.style.left = `${j * (100 / cols)}%`;
+            matrixElement.style.top = `${i * (100 / rows)}%`;
+            matrixBg.appendChild(matrixElement);
+        }
+    }
+});
